c5cd9baf24c790a05ad55698e29133403bec9161
matheuszmz <matheuszmz@hotmail.com>
2020-05-21 19:46:44 -0300

    Ajuste no Django Admin, Model Contas a pagar

--- contas/admin.py
+++ contas/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Responsavel,Conta, Contas_a_pagar, Recebimento
 
+
+class Contas_a_pagarAdmin(admin.ModelAdmin):
+    list_display = ('conta', 'responsavel', 'data_compra', 'descricao', 'valor_parcela',
+                    'data_vencimento')
+    search_fields = ('conta', 'responsavel', 'data_compra', 'descricao', 'valor_parcela',
+                    'data_vencimento')
+    list_filter = ('conta', 'responsavel', 'data_compra', 'data_vencimento')
+
+
 # Register your models here.
 admin.site.register(Responsavel)
 admin.site.register(Conta)
-admin.site.register(Contas_a_pagar)
+admin.site.register(Contas_a_pagar, Contas_a_pagarAdmin)
 admin.site.register(Recebimento)
